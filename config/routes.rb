cdd23e51bb8f45142b8516c784d5550d24e33ab9
Mercuone <ath.145bd@gmail.com>
2023-07-31 18:34:34 +0000

    Users ADD UPDATE DEL

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
+  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  root 'application#hello'
 
   # Defines the root path route ("/")
   # root "articles#index"
